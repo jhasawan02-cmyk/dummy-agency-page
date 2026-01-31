57e9d9c49e97cffdc4fa6775d0dbee7af49d340d
Sawan jha <jhasawan02@gmail.com>
2026-01-31 15:23:37 +0530

    minor styling changes

--- src/Component/Service.jsx
+++ src/Component/Service.jsx
@@ -5,13 +5,13 @@ function Service() {
   return (
     <div className='py-16 px-8 flex flex-col items-center w-full'>
       {/* Section Title */}
-      <h1 className='text-3xl font-bold uppercase tracking-widest border-b-2 border-orange-600 pb-2 mb-12 text-blue-800'>
+      <h1 className='text-3xl font-bold uppercase tracking-widest border-b-2 border-orange-600 pb-2 mb-12 shadow rotate-0.5 text-blue-800'>
         Our Services
       </h1>
 
       {/* Cards Container */}
       <div className='w-full max-w-7xl px-4 border-3 rounded-2xl border-slate-600 '>
-        <ServiceCard />
+        <ServiceCard title = "Frontend & Backend Services"/>
       </div>
     </div>
   )
--- src/Component/ServiceCard.jsx
+++ src/Component/ServiceCard.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function ServiceCard() {
   const services = ["Service 1", "Service 2", "Service 3", "Service 4"];
 
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-8">
       {services.map((service, index) => (
@@ -11,13 +12,13 @@ function ServiceCard() {
           className="h-64 border-3 cursor-pointer border-purple-950 rounded-xl p-6 bg-slate-50 text-shadow-violet-600 flex flex-col justify-between shadow-2xl hover:border-purple-400"
         >
           <div className="text-sm uppercase tracking-widest text-blue-600 font-semibold">
-            Feature {index + 1}
+            Frontend & Backend services {index + 1}
           </div>
           <div className="text-xl font-bold text-gray-800">
             {service}
           </div>
           <div className="text-sm text-gray-500 font-normal">
-            Providing reliable and responsible solutions tailored to your needs.
+            Make more use of Frontend and backend applications !
           </div>
         </div>
       ))}
